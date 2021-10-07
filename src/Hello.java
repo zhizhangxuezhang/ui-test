a08bd7d630d4474dd7adbdfdd6ea65854ce859fc
zhizhangxuezhang <jiaqi_g6@163.com>
2021-10-07 13:25:59 +0800

    我的hello

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Author jiaqi
+ * @Date 2021/10/7 13:20
+ * @Version 1.0
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
